4fb21d8d003beb1f584fc1fa3660ed37c2a6619f
Jeremias <jnahuel46@gmail.com>
2025-03-12 20:40:05 -0300

    integrate-details-repair

--- app/api/clients/route.ts
+++ app/api/clients/route.ts
@@ -7,24 +7,11 @@ export async function GET() {
   try {
     const clients = await prisma.client.findMany({
       include: {
-        _count: {
-          select: {
-            repairs: true,
-          },
-        },
+        repairs: true,
       },
     });
 
-    // Formatear la respuesta para incluir el número de arreglos en el nivel superior
-    const formattedClients = clients.map((client) => ({
-      id: client.id,
-      name: client.name,
-      phone: client.phone,
-      email: client.email,
-      repairs: client._count.repairs, // Agrega el número de arreglos
-    }));
-
-    return NextResponse.json(formattedClients);
+    return NextResponse.json(clients);
   } catch (error) {
     console.error("Error al obtener clientes:", error);
     return NextResponse.json(
